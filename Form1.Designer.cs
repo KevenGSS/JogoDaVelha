44bd6a1a8a31b95ac121f75386c896797fec8b4d
Desenvolvedor 01 <dev01@plannix.com.br>
2024-11-21 15:11:28 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace JogoDaVelha
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Variável de designer necessária.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Limpar os recursos que estão sendo usados.
+    /// </summary>
+    /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Código gerado pelo Windows Form Designer
+
+    /// <summary>
+    /// Método necessário para suporte ao Designer - não modifique 
+    /// o conteúdo deste método com o editor de código.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.button6 = new System.Windows.Forms.Button();
+      this.button7 = new System.Windows.Forms.Button();
+      this.button8 = new System.Windows.Forms.Button();
+      this.button9 = new System.Windows.Forms.Button();
+      this.btnReiniciar = new System.Windows.Forms.Button();
+      this.lblstatus = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(14, 21);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(62, 51);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(92, 21);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(62, 51);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "button2";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(170, 21);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(62, 51);
+      this.button3.TabIndex = 2;
+      this.button3.Text = "button3";
+      this.button3.UseVisualStyleBackColor = true;
+      // 
+      // button4
+      // 
+      this.button4.Location = new System.Drawing.Point(14, 78);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(62, 51);
+      this.button4.TabIndex = 3;
+      this.button4.Text = "button4";
+      this.button4.UseVisualStyleBackColor = true;
+      // 
+      // button5
+      // 
+      this.button5.Location = new System.Drawing.Point(92, 78);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(62, 51);
+      this.button5.TabIndex = 4;
+      this.button5.Text = "button5";
+      this.button5.UseVisualStyleBackColor = true;
+      // 
+      // button6
+      // 
+      this.button6.Location = new System.Drawing.Point(170, 78);
+      this.button6.Name = "button6";
+      this.button6.Size = new System.Drawing.Size(62, 51);
+      this.button6.TabIndex = 5;
+      this.button6.Text = "button6";
+      this.button6.UseVisualStyleBackColor = true;
+      // 
+      // button7
+      // 
+      this.button7.Location = new System.Drawing.Point(14, 135);
+      this.button7.Name = "button7";
+      this.button7.Size = new System.Drawing.Size(62, 51);
+      this.button7.TabIndex = 6;
+      this.button7.Text = "button7";
+      this.button7.UseVisualStyleBackColor = true;
+      // 
+      // button8
+      // 
+      this.button8.Location = new System.Drawing.Point(92, 135);
+      this.button8.Name = "button8";
+      this.button8.Size = new System.Drawing.Size(62, 51);
+      this.button8.TabIndex = 7;
+      this.button8.Text = "button8";
+      this.button8.UseVisualStyleBackColor = true;
+      // 
+      // button9
+      // 
+      this.button9.Location = new System.Drawing.Point(170, 135);
+      this.button9.Name = "button9";
+      this.button9.Size = new System.Drawing.Size(62, 51);
+      this.button9.TabIndex = 8;
+      this.button9.Text = "button9";
+      this.button9.UseVisualStyleBackColor = true;
+      // 
+      // btnReiniciar
+      // 
+      this.btnReiniciar.Location = new System.Drawing.Point(259, 21);
+      this.btnReiniciar.Name = "btnReiniciar";
+      this.btnReiniciar.Size = new System.Drawing.Size(62, 23);
+      this.btnReiniciar.TabIndex = 9;
+      this.btnReiniciar.Text = "Reiniciar";
+      this.btnReiniciar.UseVisualStyleBackColor = true;
+      this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click_1);
+      // 
+      // lblstatus
+      // 
+      this.lblstatus.AutoSize = true;
+      this.lblstatus.Location = new System.Drawing.Point(12, 202);
+      this.lblstatus.Name = "lblstatus";
+      this.lblstatus.Size = new System.Drawing.Size(35, 13);
+      this.lblstatus.TabIndex = 10;
+      this.lblstatus.Text = "label1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(330, 224);
+      this.Controls.Add(this.lblstatus);
+      this.Controls.Add(this.btnReiniciar);
+      this.Controls.Add(this.button9);
+      this.Controls.Add(this.button8);
+      this.Controls.Add(this.button7);
+      this.Controls.Add(this.button6);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Jogo da Velha";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.Button button7;
+    private System.Windows.Forms.Button button8;
+    private System.Windows.Forms.Button button9;
+    private System.Windows.Forms.Button btnReiniciar;
+    private System.Windows.Forms.Label lblstatus;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+  public partial class Form1 : Form
+  {
+    private char jogadorAtual = 'X';
+    private Button[] botoes;
+
+    public Form1()
+    {
+      InitializeComponent();
+      InicializarJogo();
+    }
+
+    private void InicializarJogo()
+    {
+      // Lista de botões para facilitar o controle
+      botoes = new Button[] { button1, button2, button3, button4, button5, button6, button7, button8, button9};
+
+      // Adicionar evento de clique a todos os botões do tabuleiro
+      foreach (var botao in botoes)
+      {
+        botao.Text = string.Empty;
+        botao.Enabled = true;
+        botao.Click += Botao_Click;
+      }
+
+      lblstatus.Text = "Vez do jogador X";
+      jogadorAtual = 'X';
+    }
+
+    private void Botao_Click(object sender, EventArgs e)
+    {
+      Button botaoClicado = (Button)sender;
+      botaoClicado.Text = jogadorAtual.ToString();
+      botaoClicado.Enabled = false;
+
+      // Verificar vitória
+      if (VerificarVitoria())
+      {
+        lblstatus.Text = $"Jogador {jogadorAtual} venceu!";
+        DesabilitarBotoes();
+        return;
+      }
+
+      // Verificar empate
+      if (VerificarEmpate())
+      {
+        lblstatus.Text = "Empate!";
+        return;
+      }
+
+      // Trocar de jogador
+      jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+      lblstatus.Text = $"Vez do jogador {jogadorAtual}";
+    }
+
+    private bool VerificarVitoria()
+{
+    // Possíveis combinações vencedoras
+    int[,] combinacoes = {
+        { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, // Linhas
+        { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, // Colunas
+        { 0, 4, 8 }, { 2, 4, 6 }              // Diagonais
+    };
+
+    for (int i = 0; i < combinacoes.GetLength(0); i++) // Iterar pelas combinações
+    {
+        if (botoes[combinacoes[i, 0]].Text == jogadorAtual.ToString() &&
+            botoes[combinacoes[i, 1]].Text == jogadorAtual.ToString() &&
+            botoes[combinacoes[i, 2]].Text == jogadorAtual.ToString())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+    private bool VerificarEmpate()
+    {
+      foreach (var botao in botoes)
+      {
+        if (string.IsNullOrEmpty(botao.Text))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    private void DesabilitarBotoes()
+    {
+      foreach (var botao in botoes)
+      {
+        botao.Enabled = false;
+      }
+    }
+
+    private void btnReiniciar_Click(object sender, EventArgs e)
+    {
+      InicializarJogo();
+    }
+
+    private void btnReiniciar_Click_1(object sender, EventArgs e)
+    {
+      this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+    }
+  }
+}
